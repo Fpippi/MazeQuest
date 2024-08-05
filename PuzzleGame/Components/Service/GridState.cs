4fa86e90d69aeaf4d0783893120b550c647aff28
Francesco Pippi <anakin.fp@gmail.com>
2024-08-05 15:20:20 +0200

    add more logic

--- PuzzleGame/Components/Service/GridState.cs
+++ PuzzleGame/Components/Service/GridState.cs
@@ -6,44 +6,73 @@ namespace MazeGame.Services
         public int[,] Grid
         {
             get => _grid;
-            set
-            {
-                _grid = value;
-            }
+            set => _grid = value;
         }
 
         public Difficulty DifficultyChosen { get; set; } = Difficulty.None;
 
-        public int[,] RandomGrid(Difficulty difficulty)
+        public int[,] GenerateRandomGrid(Difficulty difficulty)
         {
             int size = GetGridSize(difficulty);
             int[,] grid = InitializeGrid(size);
-            int accessibleCells = CalculateAccessibleCells(size);
+            int totalCells = (size - 2) * (size - 2);
+            int accessibleCellsTarget = (int)Math.Ceiling(totalCells * 0.3);
 
             Random random = new Random();
+            Stack<(int x, int y)> stack = new Stack<(int x, int y)>();
+
             int startX = random.Next(1, size - 1);
             int startY = random.Next(1, size - 1);
             grid[startX, startY] = 0;
-            accessibleCells--;
+            int accessibleCells = 1;
 
-            int currentX = startX;
-            int currentY = startY;
+            stack.Push((startX, startY));
 
-            while (IsInsideGrid(currentX, currentY, size) && accessibleCells > 0)
+            bool reachedBorder = false;
+
+            while (stack.Count > 0 && accessibleCells < accessibleCellsTarget)
             {
-                int direction = random.Next(0, 4);
-                if (TryMoveInDirection(direction, ref currentX, ref currentY, grid, ref accessibleCells))
-                {
-                    continue;
-                }
+                var (currentX, currentY) = stack.Peek();
+                var neighbors = GetAccessibleNeighbors(currentX, currentY, size, grid);
 
-                int additionalPaths = random.Next(0, 2);
-                for (int i = 0; i < additionalPaths && accessibleCells > 0; i++)
+                if (neighbors.Count > 0)
                 {
-                    int randomDirection = random.Next(0, 4);
-                    TryMoveInDirection(randomDirection, ref currentX, ref currentY, grid, ref accessibleCells);
+                    var (nextX, nextY) = neighbors[random.Next(neighbors.Count)];
+                    grid[nextX, nextY] = 0;
+                    grid[(currentX + nextX) / 2, (currentY + nextY) / 2] = 0;
+                    stack.Push((nextX, nextY));
+                    accessibleCells++;
+
+                    if (nextX == 1 || nextX == size - 2 || nextY == 1 || nextY == size - 2)
+                    {
+                        reachedBorder = true;
+                    }
+                }
+                else
+                {
+                    stack.Pop();
                 }
             }
+
+            if (!reachedBorder)
+            {
+                var (finalX, finalY) = stack.Peek();
+                ForcePathToBorder(finalX, finalY, size, grid, ref accessibleCells);
+            }
+
+            while (accessibleCells < accessibleCellsTarget)
+            {
+                int x = random.Next(1, size - 1);
+                int y = random.Next(1, size - 1);
+
+                if (grid[x, y] == 1)
+                {
+                    grid[x, y] = 0;
+                    accessibleCells++;
+                }
+            }
+
+            EnsureAccessibleBorderCell(size, grid);
 
             return grid;
         }
@@ -52,10 +81,10 @@ namespace MazeGame.Services
         {
             return difficulty switch
             {
-                Difficulty.Easy => 6,
-                Difficulty.Medium => 8,
-                Difficulty.Hard => 10,
-                _ => throw new InvalidOperationException("Difficulty not set"),
+                Difficulty.Easy => 10,
+                Difficulty.Medium => 15,
+                Difficulty.Hard => 20,
+                _ => throw new InvalidOperationException("Invalid difficulty"),
             };
         }
 
@@ -66,7 +95,7 @@ namespace MazeGame.Services
             {
                 for (int j = 0; j < size; j++)
                 {
-                    grid[i, j] = 0;
+                    grid[i, j] = 1;
                 }
             }
             for (int i = 0; i < size; i++)
@@ -76,72 +105,101 @@ namespace MazeGame.Services
                 grid[i, 0] = 1;
                 grid[i, size - 1] = 1;
             }
-            for (int i = 1; i < size - 1; i++)
-            {
-                for (int j = 1; j < size - 1; j++)
-                {
-                    grid[i, j] = 1;
-                }
-            }
             return grid;
         }
 
-        private int CalculateAccessibleCells(int size)
+        private List<(int x, int y)> GetAccessibleNeighbors(int x, int y, int size, int[,] grid)
         {
-            return (int)Math.Ceiling((size - 2) * (size - 2) * 0.4);
+            var neighbors = new List<(int, int)>();
+            if (x - 2 > 0 && grid[x - 2, y] == 1) neighbors.Add((x - 2, y));
+            if (x + 2 < size - 1 && grid[x + 2, y] == 1) neighbors.Add((x + 2, y));
+            if (y - 2 > 0 && grid[x, y - 2] == 1) neighbors.Add((x, y - 2));
+            if (y + 2 < size - 1 && grid[x, y + 2] == 1) neighbors.Add((x, y + 2));
+            return neighbors;
+        }
+
+        private void ForcePathToBorder(int x, int y, int size, int[,] grid, ref int accessibleCells)
+        {
+            Random random = new Random();
+
+            while (!(x == 1 || x == size - 2 || y == 1 || y == size - 2))
+            {
+                var directions = new List<(int, int)>
+                    {
+                        (x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)
+                    };
+
+                var validDirections = directions.Where(d => IsInsideGrid(d.Item1, d.Item2, size) && grid[d.Item1, d.Item2] == 1).ToList();
+
+                if (validDirections.Count == 0) break;
+
+                var (nextX, nextY) = validDirections[random.Next(validDirections.Count)];
+
+                grid[nextX, nextY] = 0;
+                grid[(x + nextX) / 2, (y + nextY) / 2] = 0;
+                x = nextX;
+                y = nextY;
+                accessibleCells++;
+            }
         }
 
         private bool IsInsideGrid(int x, int y, int size)
         {
-            return x != 0 && x != size - 1 && y != 0 && y != size - 1;
+            return x > 0 && x < size - 1 && y > 0 && y < size - 1;
         }
 
-        private bool TryMoveInDirection(int direction, ref int x, ref int y, int[,] grid, ref int accessibleCells)
+        private void EnsureAccessibleBorderCell(int size, int[,] grid)
         {
-            switch (direction)
+            Random random = new Random();
+            List<(int x, int y)> borderCells = new List<(int x, int y)>();
+
+            for (int i = 1; i < size - 1; i++)
             {
-                case 0: // Up
-                    if (x - 2 >= 0 && grid[x - 2, y] == 1)
-                    {
-                        grid[x - 1, y] = 0;
-                        grid[x - 2, y] = 0;
-                        x -= 2;
-                        accessibleCells--;
-                        return true;
-                    }
-                    break;
-                case 1: // Right
-                    if (y + 2 < grid.GetLength(1) && grid[x, y + 2] == 1)
-                    {
-                        grid[x, y + 1] = 0;
-                        grid[x, y + 2] = 0;
-                        y += 2;
-                        accessibleCells--;
-                        return true;
-                    }
-                    break;
-                case 2: // Down
-                    if (x + 2 < grid.GetLength(0) && grid[x + 2, y] == 1)
-                    {
-                        grid[x + 1, y] = 0;
-                        grid[x + 2, y] = 0;
-                        x += 2;
-                        accessibleCells--;
-                        return true;
-                    }
-                    break;
-                case 3: // Left
-                    if (y - 2 >= 0 && grid[x, y - 2] == 1)
-                    {
-                        grid[x, y - 1] = 0;
-                        grid[x, y - 2] = 0;
-                        y -= 2;
-                        accessibleCells--;
-                        return true;
-                    }
-                    break;
+                borderCells.Add((0, i));
+                borderCells.Add((size - 1, i));
+                borderCells.Add((i, 0));
+                borderCells.Add((i, size - 1));
             }
-            return false;
+
+            while (borderCells.Count > 0)
+            {
+                var (x, y) = borderCells[random.Next(borderCells.Count)];
+                if (grid[x, y] == 1)
+                {
+                    grid[x, y] = 0;
+                    CreatePathToBorder(x, y, size, grid);
+                    break;
+                }
+                borderCells.Remove((x, y));
+            }
+        }
+
+        private void CreatePathToBorder(int x, int y, int size, int[,] grid)
+        {
+            Random random = new Random();
+            List<(int dx, int dy)> directions = new List<(int, int)>
+                {
+                    (1, 0), (-1, 0), (0, 1), (0, -1)
+                };
+
+            while (true)
+            {
+                var (dx, dy) = directions[random.Next(directions.Count)];
+                int newX = x + dx;
+                int newY = y + dy;
+
+                if (IsInsideGrid(newX, newY, size) && grid[newX, newY] == 1)
+                {
+                    grid[newX, newY] = 0;
+                    grid[(x + newX) / 2, (y + newY) / 2] = 0;
+                    x = newX;
+                    y = newY;
+                }
+                else if (!IsInsideGrid(newX, newY, size))
+                {
+                    break;
+                }
+            }
         }
     }
 
