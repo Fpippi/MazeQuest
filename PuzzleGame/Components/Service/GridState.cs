1584b9ada4215fda814539e61aacd8352e0790b1
Francesco Pippi <anakin.fp@gmail.com>
2024-08-05 11:51:22 +0200

    2024 08 05 create Difficulty form

--- PuzzleGame/Components/Service/GridState.cs
+++ PuzzleGame/Components/Service/GridState.cs
@@ -2,14 +2,74 @@ namespace MazeGame.Services
 {
     public class GridState
     {
-        public  int[,] Grid = new int[,]
+        private int[,] _grid;
+        public int[,] Grid
         {
-        { 1, 1, 1, 1, 1, 1 },
-        { 1, 0, 0, 0, 0, 1 },
-        { 1, 0, 1, 1, 0, 1 },
-        { 1, 0, 1, 0, 0, 1 },
-        { 1, 0, 0, 0, 1, 1 },
-        { 1, 1, 1, 0, 1, 1 }
-        };
+            get => _grid;
+            set
+            {
+                _grid = value;
+                NotifyStateChanged();
+            }
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+        public delegate void StateChangedHandler();
+        public event StateChangedHandler OnChange;
+
+        public required Difficulty DifficultyChosen { get; set; } = Difficulty.None;
+        public int[,] RandomGrid(Difficulty difficulty)
+        {
+            int size = 6;
+
+            switch (difficulty)
+            {
+                case Difficulty.Easy:
+                    size = 6;
+                    break;
+                case Difficulty.Medium:
+                    size = 8;
+                    break;
+                case Difficulty.Hard:
+                    size = 10;
+                    break;
+                case Difficulty.None:
+                    size = 6;
+                    break;
+            }
+
+            int[,] grid = new int[size, size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    grid[i, j] = 0;
+                }
+            }
+            for (int i = 0; i < size; i++)
+            {
+                grid[0, i] = 1;
+                grid[size - 1, i] = 1;
+                grid[i, 0] = 1;
+                grid[i, size - 1] = 1;
+            }
+            for (int i = 1; i < size - 1; i++)
+            {
+                for (int j = 1; j < size - 1; j++)
+                {
+                    grid[i, j] = 1;
+                }
+            }
+            return grid;
+        }
+    }
+
+    public enum Difficulty
+    {
+        None,
+        Easy,
+        Medium,
+        Hard
     }
 }
